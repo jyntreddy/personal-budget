0f25835ca10645cd0383c66433b953eb45a3790c
jayanth eswar reddy <jayantheswarr@gmail.com>
2023-09-17 22:11:22 +0530

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,36 +1,52 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const data = require("./budget-data.json");
+const data = require("./server.json");
 
 app.use('/', express.static('public'));
 
 const budget = {
-    myBudget: [
+    myBudget:[
     {
-        title: 'Eat Out',
-        budget: 25
+        title: 'Eat out',
+        budget: 300
     },
     {
         title: 'Rent',
-        budget: 375
+        budget: 350
     },
     {
-        title: 'Grocery',
-        budget: 110
+        title: 'Gas',
+        budget: 85
     },
-  ]
+    {
+        title: 'Groceries',
+        budget: 30
+    },
+    {
+        title: 'clothing',
+        budget: 250
+    },
+    {
+        title: 'Traveling',
+        budget: 450
+    },
+    {
+        title: 'Electronics',
+        budget: 359
+    },
+    
+]
 };
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req, res) => {
+    res.send('hello world!');
 });
 
-app.get('/budget', (req, res) => {
-    res.json(budget);
-});
-
+app.use('/budget', (req, res) => {
+    res.sendFile("C:\\Users\\jayanth eswar reddy\\Desktop\\NBAD\\WEEK03\\personal-budget1\\server.json");
+})
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+    console.log(`Example app listening at http://localhost:${port}`)
 });
